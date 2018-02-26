9fbd261a2a25117a8ae03863fd9be0aceb0d1fc5
aliciareneesnow <35249110+aliciareneesnow@users.noreply.github.com>
2018-02-26 14:52:35 -0700

    updated

--- assignments/lesson-8/js/currentdate.js
+++
@@ -1,7 +0,0 @@
-var dayname = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-
-var monthname = new Array("January","February","March","April","May","June","July","August","September","October","November",
-"December");
-var d=new Date();
-var today = dayname[d.getDay()] + ", " + d.getDate() + " " + monthname[d.getMonth()] + " " + d.getFullYear();
-document.getElementById("currentdate").innerHTML = today;
--- assignments/lesson-8/js/hamburger.js
+++
@@ -1,3 +0,0 @@
-function toggleNavMenu() {
-   document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
