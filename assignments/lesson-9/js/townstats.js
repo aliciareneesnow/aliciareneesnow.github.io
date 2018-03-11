1d911009a7d9813dd8bec88927186fd095720104
aliciareneesnow <35249110+aliciareneesnow@users.noreply.github.com>
2018-03-10 22:25:28 -0700

    Changes

--- assignments/lesson-9/js/townstats.js
+++ assignments/lesson-9/js/townstats.js
@@ -15,20 +15,20 @@ function populateTownData(jsonObj) {
     for (var i = 0; i < towns.length; i++) {
         if (towns[i].name == 'Franklin' || towns[i].name == 'Greenville' || towns[i].name == 'Springfield') {
             var myArticle = document.getElementById(towns[i].name);
-            var myH2 = document.createElement('h2');
+            var myH3 = document.createElement('h3');
             var myPara1 = document.createElement('p');
             var myPara2 = document.createElement('p');
             var myPara3 = document.createElement('p');
             var myPara4 = document.createElement('p');
             var myList = document.createElement('ul');
 
-            myH2.textContent = towns[i].name;
-            myPara1.textContent = 'Motto: ' + towns[i].motto;
+            myH3.textContent = towns[i].name;
+            myPara1.textContent = '"' + towns[i].motto + '"';
             myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            myPara3.textContent = 'Current Population: ' + towns[i].currentPopulation;
+            myPara3.textContent = 'Population: ' + towns[i].currentPopulation;
             myPara4.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
 
-            myArticle.appendChild(myH2);
+            myArticle.appendChild(myH3);
             myArticle.appendChild(myPara1);
             myArticle.appendChild(myPara2);
             myArticle.appendChild(myPara3);
