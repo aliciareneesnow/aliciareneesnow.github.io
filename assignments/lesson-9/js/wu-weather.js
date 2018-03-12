2a417825a925effdbf5b73b06df39f1cadf454e1
aliciareneesnow <35249110+aliciareneesnow@users.noreply.github.com>
2018-03-11 21:54:45 -0700

    trial

--- assignments/lesson-9/js/wu-weather.js
+++ assignments/lesson-9/js/wu-weather.js
@@ -1,16 +1,19 @@
 var weatherFranklin = new XMLHttpRequest();
-weatherFranklin.open('GET', 'https://api.wunderground.com/api/15342b83d4c08511/conditions/q/MN/Franklin.json', true);
+weatherFranklin.open('GET', 'https://api.wunderground.com/api/512fa2acfaad275a/conditions/q/MN/Franklin.json', true);
 weatherFranklin.send();
 
 weatherFranklin.onload = function() {
   var wuWeather = JSON.parse(weatherFranklin.responseText);
   console.log(wuWeather);
-
-  document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.city;
+    
   document.getElementById('w-icon').src = weatherInfo.current_observation.icon_url;
   document.getElementById('weatherString').innerHTML = weatherInfo.current_observation.weather;
   document.getElementById('highT').innerHTML = weatherInfo.current_observation.temp_f;
-  document.getElementById('windSpeed').innerHTML = weatherInfo.current_observation.wind_mph;
-  document.getElementById('precip').innerHTML = weatherInfo.current_observation.precip_1hr_in;
-  document.getElementById('windChill').innerHTML = weatherInfo.current_observation.windchill_f;
+  document.getElementById('windS').innerHTML = weatherInfo.current_observation.wind_mph;
+  document.getElementById('precipitation').innerHTML = weatherInfo.current_observation.precip_1hr_in;
+  document.getElementById('windchill').innerHTML = weatherInfo.current_observation.windchill_f;
+
+var iconOrig = franklinWeather.current_observation.icon_url;
+    var iconReplace = iconOrig.replace("http", "https");
+    document.getElementById('current-weather-icon').src = iconReplace;
 }
