c07d159967a083c0ce094063ae4f29e46a0faf34
aliciareneesnow <35249110+aliciareneesnow@users.noreply.github.com>
2018-03-11 22:27:22 -0700

    trial3

--- assignments/lesson-9/js/wu-weather.js
+++ assignments/lesson-9/js/wu-weather.js
@@ -1,15 +1,29 @@
-var franklinWeather = new XMLHttpRequest();
-franklinWeather.open('GET', 'https://api.wunderground.com/api/512fa2acfaad275a/conditions/q/MN/Franklin.json', true);
-franklinWeather.send();
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', 'https://api.wunderground.com/api/512fa2acfaad275a/conditions/q/MN/Franklin.json', true);
+weatherObject.send();
 
-franklinWeather.onload = function() {
-    var weatherFranklin = JSON.parse(franklinWeather.responseText);
-    document.getElementById('highT').innerHTML = franklinWeather.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
-    document.getElementById('lowT').innerHTML = franklinWeather.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
-    document.getElementById('precipitation').innerHTML = parseInt(franklinWeather.current_observation.precip_today_in);
-    document.getElementById('wind-speed').innerHTML = franklinWeather.current_observation.wind_mph;
-    document.getElementById('conditions').innerHTML = franklinWeather.current_observation.weather;
-    document.getElementById('currentweather').innerHTML = franklinWeather.current_observation.temp_f;
-    document.getElementById('condition-text').innerHTML = franklinWeather.forecast.txt_forecast.forecastday["0"].fcttext;
+weatherObject.onload = function() {
+    var weatherFranklin = JSON.parse(weatherObject.responseText);
+    console.log(weatherFranklin);
 
+    document.getElementById('windSpeed').innerHTML = weatherFranklin.current_observation.wind_mph;
+    document.getElementById('windChill').innerHTML = weatherFranklin.current_observation.windchill_f;
+    document.getElementById('currentTemp').innerHTML = weatherFranklin.current_observation.temp_f;
+    document.getElementById('precipitation').innerHTML = weatherFranklin.current_observation.precip_today_in;
+    document.getElementById('currentWeather').innerHTML = weatherFranklin.current_observation.weather;
+    document.getElementById('w_icon').src = weatherFranklin.current_observation.icon_url.replace('http:', 'https:');;
+}
+
+var weatherObject2 = new XMLHttpRequest();
+
+weatherObject2.open('GET', 'https://api.wunderground.com/api/512fa2acfaad275a/conditions/q/MN/Franklin.json', true);
+weatherObject2.send();
+
+weatherObject2.onload = function() {
+    var weatherFranklinForecast = JSON.parse(weatherObject2.responseText);
+    console.log(weatherFranklinForecast);
+
+    document.getElementById('textForecast').innerHTML = weatherFranklinForecast.forecast.txt_forecast.forecastday["0"].fcttext;
+    document.getElementById('lowT').innerHTML = weatherFranklinForecast.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
+    document.getElementById('highT').innerHTML = weatherFranklinForecast.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
 }
