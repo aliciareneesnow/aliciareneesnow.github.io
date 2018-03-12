ed4b2c5c5dc32510cc271435d8a866ae057028c6
aliciareneesnow <35249110+aliciareneesnow@users.noreply.github.com>
2018-03-11 22:08:48 -0700

    trial2

--- assignments/lesson-9/js/wu-weather.js
+++ assignments/lesson-9/js/wu-weather.js
@@ -1,19 +1,15 @@
-var weatherFranklin = new XMLHttpRequest();
-weatherFranklin.open('GET', 'https://api.wunderground.com/api/512fa2acfaad275a/conditions/q/MN/Franklin.json', true);
-weatherFranklin.send();
+var franklinWeather = new XMLHttpRequest();
+franklinWeather.open('GET', 'https://api.wunderground.com/api/512fa2acfaad275a/conditions/q/MN/Franklin.json', true);
+franklinWeather.send();
 
-weatherFranklin.onload = function() {
-  var wuWeather = JSON.parse(weatherFranklin.responseText);
-  console.log(wuWeather);
-    
-  document.getElementById('w-icon').src = weatherInfo.current_observation.icon_url;
-  document.getElementById('weatherString').innerHTML = weatherInfo.current_observation.weather;
-  document.getElementById('highT').innerHTML = weatherInfo.current_observation.temp_f;
-  document.getElementById('windS').innerHTML = weatherInfo.current_observation.wind_mph;
-  document.getElementById('precipitation').innerHTML = weatherInfo.current_observation.precip_1hr_in;
-  document.getElementById('windchill').innerHTML = weatherInfo.current_observation.windchill_f;
+franklinWeather.onload = function() {
+    var weatherFranklin = JSON.parse(franklinWeather.responseText);
+    document.getElementById('highT').innerHTML = franklinWeather.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
+    document.getElementById('lowT').innerHTML = franklinWeather.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
+    document.getElementById('precipitation').innerHTML = parseInt(franklinWeather.current_observation.precip_today_in);
+    document.getElementById('wind-speed').innerHTML = franklinWeather.current_observation.wind_mph;
+    document.getElementById('conditions').innerHTML = franklinWeather.current_observation.weather;
+    document.getElementById('currentweather').innerHTML = franklinWeather.current_observation.temp_f;
+    document.getElementById('condition-text').innerHTML = franklinWeather.forecast.txt_forecast.forecastday["0"].fcttext;
 
-var iconOrig = franklinWeather.current_observation.icon_url;
-    var iconReplace = iconOrig.replace("http", "https");
-    document.getElementById('current-weather-icon').src = iconReplace;
 }
