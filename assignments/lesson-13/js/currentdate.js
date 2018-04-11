61342adc1713ebfadc8a90cbf2fd41ae334d2954
aliciareneesnow <35249110+aliciareneesnow@users.noreply.github.com>
2018-04-11 11:16:25 -0700

    hhh

--- assignments/lesson-13/js/currentdate.js
+++
@@ -1,7 +0,0 @@
-var dayname = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-
-var monthname = new Array("January","February","March","April","May","June","July","August","September","October","November",
-"December");
-var d=new Date();
-var today = dayname[d.getDay()] + ", " + monthname[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
-document.getElementById("currentdate").innerHTML = today;
